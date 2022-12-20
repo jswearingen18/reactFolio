3f8f17cdcfbaf2980dc99d9490516fcd1b8713eb
Jeremiah Swearingen <jswearingen18@gmail.com>
2022-12-19 18:33:59 -0600

    work on header and navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/Navbar.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <a href="/" className="fa fa-github"></a>
+      <a href="/" className="fa fa-linkedin"></a>
+    </nav>
+  );
+}
+
+export default Navbar;
